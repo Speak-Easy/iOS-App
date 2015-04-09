5bb14e077a1179a012f1c779e8f2e2f8f0f4765c
Bryce Langlotz <BryceLanglotzCS@gmail.com>
2015-04-09 13:27:46 -0400

    Universal Color
    Changed tint color to AlgorithmsGreen so there is no need to manually
    change button tint color.

--- Reviewify MVP/Reviewify MVP/AppDelegate.swift
+++ Reviewify MVP/Reviewify MVP/AppDelegate.swift
@@ -25,6 +25,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         PFAnalytics.trackAppOpenedWithLaunchOptionsInBackground(launchOptions, block: nil)
         
+        window?.tintColor = UIColor.algorithmsGreen()
+        
         return true
     }
 
